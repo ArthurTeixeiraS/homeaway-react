4396be6ec6a270a93ea462a86b88de3eb94deca1
Arthur Teixeira Serafim <arthurteixeiraserafim@gmail.com>
2023-11-13 06:28:55 -0300

    mudança da porta do servidor-cliente

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8080',
-    },
-  },
   plugins: [react()],
+  server: {
+    port: 3006,
+  },
 })
